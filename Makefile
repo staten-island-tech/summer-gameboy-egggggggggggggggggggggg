032168d02bcfa51cfcc01e6d8d21228344821422
johnnyw <144480382+egggggggggggggggggggggg@users.noreply.github.com>
2026-07-21 19:05:36 -0400

    example stuff

---
+++ Makefile
@@ -0,0 +1,5 @@
+CC = lcc 
+CFLAGS = 
+
+all : 
+	$(CC) -o main.gb src/main.c
--- src/main.c
+++ src/main.c
@@ -1,4 +1,17 @@
 #include <gb/gb.h>
-int main(int argc, char *argv[]) {
-    return -1;
+#include <stdint.h>
+
+void scroll(void) {}
+
+void main(void) {
+    uint8_t i, j;
+    disable_interrupts();
+    DISPLAY_OFF;
+    LCDC_REG = LCDCF_OFF | LCDCF_WIN9C00 | LCDCF_WINON | LCDCF_BG8800 | LCDCF_BG9800 | LCDCF_OBJ16 |
+               LCDCF_OBJON | LCDCF_BGON;
+
+    DISPLAY_ON;
+    enable_interrupts();
+    while (1) {
+    }
 }
